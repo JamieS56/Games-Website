69c13c65c8f8be3010c97c879693e59225372473
James SEBIRE <james.sebire@btinternet.com>
2022-02-01 16:03:49 +0000

    first commit

---
+++ frontend/src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { render } from "react-dom";
+
+function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+  }
+  
+setInterval(tick, 1000);
+

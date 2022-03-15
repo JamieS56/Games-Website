8cf87e009cede29a8314dff01fc612f31004d283
James SEBIRE <james.sebire@btinternet.com>
2022-03-15 16:28:12 +0000

    Calling API from react button clickgit add .!

--- gamesWebsite/urls.py
+++ gamesWebsite/urls.py
@@ -21,19 +21,12 @@ from django.conf.urls.static import static
 
 from django.urls import include, path
 from rest_framework import routers
-from gamesWebsite.profiles import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-
-
+    path('users/', include('profiles.urls'))
 
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
